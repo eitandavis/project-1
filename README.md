980a9b47e44a374c997d949eb9dee88c5c7dfe91
Eitan Davis <edavis9@uoregon.edu>
2020-04-07 15:27:01 -0700

    Add Objectives for README

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 - Basic Unix Shell commands
 - Install and Configure Git using the Unix Shell
+- Basic Git Commands (add, commit, push, clone)
 - Github Overview
+- Markdown language for README files
+- JavaScript ES6+ features
+- Understand different structures of arrow functions
---
+++ javascripts/functions.js
@@ -0,0 +1,27 @@
+//jshint esversion: 7
+
+//Req. 3B
+let arrAvg = (arr) => {
+  let sum = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    //array[i]
+  }
+};
+
+//Req. 3C
+let arrMax = (arr) => {
+  //
+};
+
+let fruits = ["Apple", "Orange", "Plum"];
+let maxSoFar = fruits[0].length;
+
+//iterate over array ay
+fruits.forEach((elem) => {
+  if(elem.length > maxSoFar) {
+    maxSoFar = elem.length;
+  }
+});
+
+console.log(maxSoFar);
