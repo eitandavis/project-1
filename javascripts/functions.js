ebdcef25080f979c2d36ebb313b3457e581ee5d7
Eitan Davis <edavis9@uoregon.edu>
2020-04-08 10:11:25 -0700

    Add arrMax to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,7 +1,7 @@
 //jshint esversion: 7
 
 //Req. 3B
-let arrAvg = (arr) => {
+let arrAvg = arr => {
   let sum = 0;
 
   for (var i = 0; i < arr.length; i++) {
@@ -12,18 +12,15 @@ let arrAvg = (arr) => {
 };
 
 //Req. 3C
-let arrMax = (arr) => {
-  //
+let arrMax = arr => {
+  let largest = 0;
+  arr.forEach(elem => {
+    if (largest < elem) {
+      largest = elem;
+      {
+        return largest;
+      }
+    }
+  });
+  console.log(largest);
 };
-
-let fruits = ["Apple", "Orange", "Plum"];
-let maxSoFar = fruits[0].length;
-
-//iterate over array ay
-fruits.forEach((elem) => {
-  if(elem.length > maxSoFar) {
-    maxSoFar = elem.length;
-  }
-});
-
-console.log(maxSoFar);
